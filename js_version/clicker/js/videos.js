5a26f172887dee8cbb7c4c0f227e9729a88eca79
tomnya <yanxin09nju@gmail.com>
2013-10-15 22:12:55 +0800

    js version 2 pushlished

--- js_version/clicker/js/videos.js
+++ js_version/clicker/js/videos.js
@@ -1,42 +1,93 @@
-var links = $('a');
-var i = 0;
-var count = 0;
-var promptFlag = true;
+var j = 0;
+var downloadLinks = new Array();
 
 var extractFunc = function() {
-
+	
+	var titles = $('.course-item-list-header h3');
+	console.log(titles);
+	var titleStr = '';
+	for(var i=0; i<titles.length; i++) {
+		var title = $(titles[i]).text();
+		console.log(title);
+		var no = i + 1;
+		titleStr += no + title + '\n';
+	}
+	return titleStr;
 };
 
-var mapFunc = function() {
-
+var mapFunc = function(downloadNo) {
+	
+	var sections = $('.course-item-list-section-list');
+	var countArr = new Array();
+	var acc = 0;
+	for(var i=0; i<sections.length; i++) {
+		var items = $(sections[i]).children('li');
+		acc += items.length;
+		console.log(acc);
+		countArr.push(acc);
+	}
+	console.log(countArr);
+	
+	var limits = downloadNo.split('-');
+	var first = parseInt(limits[0]);
+	var last = parseInt(limits[1]);
+	
+	var firstCount = 0;
+	if( first == 1) 
+		firstCount = 1;
+	else 
+		firstCount = countArr[first-2]+1;
+	var lastCount = countArr[last-1];
+	console.log(firstCount);
+	console.log(lastCount);
+	
+	var links = $('a');
+	var mapLinks = new Array();
+	var count = 0;
+	for( var i=0; i<links.length; i++) {
+		var url = $(links[i]).attr('href') + '';
+		var flag = false;
+		if( url.indexOf('mp4')>0 ) {
+			count++;
+			flag = true;
+		}
+		if( count >= firstCount && flag) {
+			mapLinks.push(url);
+		}
+		if( count > lastCount) {
+			break;
+		}
+	}
+	
+	console.log(mapLinks);
+	return mapLinks;
 };
 
 var downloadFunc = function() {
+	
+	for( ; j<downloadLinks.length; ) {
+		var url = downloadLinks[j];
+		window.open(url);
+		setTimeout('downloadFunc()', 1000);
+		j++;
+		return;
+		
+	}
 
 };
 
 var clickFunc = function() {
+
+	var lectureInfo = extractFunc();
 	
-	if ( promptFlag == true) {
-		var lectureInfo = 'line\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\nline\n\n';
-		var inputInfo = '输入起止编号, 以"-"分隔, 如1-13';
-		var hintMessage = lectureInfo + inputInfo;
-		var downloadNo = prompt(hintMessage,'1-13');
-		console.log(downloadNo);
-		promptFlag = false;
-	}
+	var inputInfo = '输入起止编号, 以"-"分隔, 如1-10';
+	var hintMessage = lectureInfo + '\n' + inputInfo;
+	var downloadNo = prompt(hintMessage,'1-10');
+	console.log(downloadNo);
 	
-	for( ; i<links.length; i++) {
-		var url = $(links[i]).attr('href');
-		url = url+'';
-		if(url.indexOf('mp4')>0 && count<1){
-			window.open(url);
-			console.log(url);
-			count++;
-			setTimeout('clickFunc()', 2000);
-			i++;
-			return;
-		}	
-	}
+	downloadLinks = mapFunc(downloadNo);
+	
+	downloadFunc();
+
 }
 clickFunc();
